7e09d9285be99137dac4d0cb59ae0ded899ab63a
Sujai Kumar <sujaikumar@users.noreply.github.com>
2017-05-28 16:30:55 +0100

    Update run_genetrees.pl

--- compara/run_genetrees.pl
+++ compara/run_genetrees.pl
@@ -1,83 +1,27 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl -w
 
 use strict;
-use warnings;
-use File::Path qw(make_path);
-use File::Find;
+use File::Basename;
 use EasyImport::Core;
-use EasyImport::Compara;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
 
 ## load parameters from an INI-style config file
 my %sections = (
-  'TAXA' => {},
-  'SETUP' => {
-    'FASTA_DIR' => 1,
-    'MAFFT' => 1,
-    'NOISY' => 1,
-    'RAXML' => 1,
-    'NOTUNG' => 1,
-    'NOTUNG_SPECIESTREE' => 1,
-    'JAVA' => 1,
-  },
-  'ORTHOGROUP' => {
-    'PREFIX' => 1,
-    'SUFFIXLENGTH' => 1,
-    'PROTEIN' => 1,
-    'PROTEIN_ALIGN' => 1,
-    'PROTEIN_TRIMMED' => 1,
-    'FNAFILE' => 1,
-    'BOUNDEDFILE' => 1,
-    'TREE' => 1,
-    'HOMOLOG' => 1,
-  },
-  'SPECIES_SET' => {
-    'TREE_FILE' => 1,
-  },
-);
-
+  'ORTHOGROUP'  => {
+    'PREFIX'       => 1
+  }
+  );
 ## check that all required parameters have been defined in the config file
 die "ERROR: you must specify at least one ini file\n",usage(),"\n" unless $ARGV[0];
 my %params;
 my $params = \%params;
 while (my $ini_file = shift @ARGV){
-  load_ini($params,$ini_file,\%sections,scalar(@ARGV));
+	load_ini($params,$ini_file,\%sections,scalar(@ARGV));
 }
-
 
 #===============================================================================
 
-my $orthogroup_prefix       = $params->{'ORTHOGROUP'}{'PREFIX'};
-
-open QSUB, ">run_genetrees_qsub.txt" or die $!;
-
-find({wanted => sub {
-  my $file = $File::Find::name;
-  if (!-d $file){
-    if ($file =~ m/$orthogroup_prefix\w+$/){
-      warn "creating genetree qsub script for $file\n";
-      create_qsub_script($params, $file);
-      print QSUB "$file.bash\n";
-    }
-  }
-},
-no_chdir => 1},'.');
-
-close QSUB;
-open  QSUB, ">qsub.bash" or die $!;
-print QSUB  '
-#$ -j y
-#$ -pe smp 2
-export MAFFT='. $params->{'SETUP'}{'MAFFT'}.'
-export RAXML='. $params->{'SETUP'}{'RAXML'}.'
-export NOISY='. $params->{'SETUP'}{'NOISY'}.'
-export NOTUNG='. $params->{'SETUP'}{'NOTUNG'}.'
-export NOTUNG_SPECIESTREE='. $params->{'SETUP'}{'NOTUNG_SPECIESTREE'}.'
-export COMPARATEMP='. $params->{'SETUP'}{'COMPARATEMP'}.'
-export JAVA='. $params->{'SETUP'}{'JAVA'}.'
-awk "NR==$SGE_TASK_ID" run_genetrees_qsub.txt | bash
-';
-
-#system "qsub -l h=bigshot|bigwig|bigbird|bigbang -cwd -V -t 1-`cat run_genetrees_qsub.txt | wc -l` qsub.bash";
+system "parallel ORTHOGROUPID={/} /ensembl/easy-import/compara/genetree.sh ::: orthogroups/".$params->{'ORTHOGROUP'}{'PREFIX'}."*"
 
 sub usage {
   return "USAGE: perl /path/to/run_genetrees.pl /path/to/config_file.ini";
